7d2d396df3f741b28eafd3642a4878a75b79d3d5
TheVoidNebula <75329526+TheVoidNebula@users.noreply.github.com>
2021-02-06 21:42:47 +0100

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -12,32 +12,32 @@ namespace MapControl
     {
 
         [Description("Is this plugin enabled?")]
-        public bool IsEnabled = true;
+        public bool IsEnabled { get; set; } = true;
 
         [Description("Are tesla gates enabled?")]
-        public bool TeslaGatesEnabled = true;
+        public bool TeslaGatesEnabled { get; set; } = true;
 
         [Description("Should random tesla gate timeouts happen?")]
-        public bool RandomTeslaTimeouts = true;
+        public bool RandomTeslaTimeouts { get; set; } = true;
 
         [Description("Should Gate A and Gate B sometimes be randomly locked?")]
-        public bool RandomGatelockdowns = true;
+        public bool RandomGatelockdowns { get; set; } = true;
 
         [Description("Should Gate A and Gate B be locked at the beginning of the round?")]
-        public bool RoundStartGatelockdown = true;
+        public bool RoundStartGatelockdown { get; set; } = true;
 
         [Description("Should Tesla gates be disabled if the player has a certain item in his inventory?")]
-        public bool TeslaGateBypassItemsEnabled = true;
+        public bool TeslaGateBypassItemsEnabled { get; set; } = true;
 
         [Description("Which items in the inventory of a player should not trigger tesla gates?")]
-        public List<int> TeslaBypassItems = new List<int>()
+        public List<int> TeslaBypassItems { get; set; } = new List<int>()
         {
             11,
             19
         };
 
         [Description("Which classes should not trigger tesla gates?")]
-        public List<int> TeslaBypassClasses = new List<int>()
+        public List<int> TeslaBypassClasses { get; set; } = new List<int>()
         {
             4,
             6,
@@ -48,66 +48,66 @@ namespace MapControl
         };
 
         [Description("How long should Gate A and Gate B be locked at the beginning of the round?")]
-        public float RoundStartGatelockdownDuration = 120f;
+        public float RoundStartGatelockdownDuration { get; set; } = 120f;
 
         [Description("How long should the Gatelockdown at the beginning of the round should be delayed? (Important to prevent broadcast spam at the start of the round)")]
-        public float RoundStartGatelockdownDelay = 10f;
+        public float RoundStartGatelockdownDelay { get; set; } = 10f;
 
         [Description("Should a broadcast be shown when a Gatelockdown happens?")]
-        public bool GatelockdownBroadcastEnabled = true;
+        public bool GatelockdownBroadcastEnabled { get; set; } = true;
 
         [Description("Should a C.A.S.S.I.E announcement be shown when a Gatelockdown happens?")]
-        public bool GatelockdownCassieEnabled = true;
+        public bool GatelockdownCassieEnabled { get; set; } = true;
 
         [Description("Should a broadcast be shown when a Tesla timeout happens?")]
-        public bool TeslaTimeoutBroadcastEnabled = true;
+        public bool TeslaTimeoutBroadcastEnabled { get; set; } = true;
 
         [Description("Should a C.A.S.S.I.E announcement be shown when a Tesla timeout happens?")]
-        public bool TeslaTimeoutCassieEnabled = true;
+        public bool TeslaTimeoutCassieEnabled { get; set; } = true;
 
         [Description("What should the normal Gatelockdown Broadcast show?")]
-        public string GatelockdownBroadcast = "<b>The gates to surface have been locked down!</b>";
+        public string GatelockdownBroadcast { get; set; } = "<b>The gates to surface have been locked down!</b>";
 
         [Description("What should the normal Gatelockdown C.A.S.S.I.E announcement show?")]
-        public string GatelockdownCassie = "The gates to surface have been locked down";
+        public string GatelockdownCassie { get; set; } = "The gates to surface have been locked down";
 
         [Description("What should the normal Gatelockdown Broadcast show when the gates are being unlocked?")]
-        public string GatelockdownEndingBroadcast = "<b>The gates to surface are no longer locked down!</b>";
+        public string GatelockdownEndingBroadcast { get; set; } = "<b>The gates to surface are no longer locked down!</b>";
 
         [Description("What should the normal Gatelockdown C.A.S.S.I.E announcement show?")]
-        public string GatelockdownEndingCassie = "The gates to surface are no longer locked down";
+        public string GatelockdownEndingCassie { get; set; } = "The gates to surface are no longer locked down";
 
         [Description("What should the normal Tesla Timeout Broadcast show?")]
-        public string TeslaTimeoutBroadcast = "<b>The tesla gates have a timeout!</b>";
+        public string TeslaTimeoutBroadcast { get; set; } = "<b>The tesla gates have a timeout!</b>";
 
         [Description("What should the normal Tesla Timout C.A.S.S.I.E announcement show?")]
-        public string TeslaTimeoutCassie = "The facility tesla gate. are disabled";
+        public string TeslaTimeoutCassie { get; set; } = "The facility tesla gate. are disabled";
 
         [Description("What should the normal Tesla Timout Broadcast show when the gates are being unlocked?")]
-        public string TeslaTimeoutEndingBroadcast = "<b>The tesla gates have no longer a timeout!</b>";
+        public string TeslaTimeoutEndingBroadcast { get; set; } = "<b>The tesla gates have no longer a timeout!</b>";
 
         [Description("What should the normal Tesla Timout C.A.S.S.I.E announcement show?")]
-        public string TeslaTimeoutEndingCassie = "The facility tesla gate. are no longer disabled";
+        public string TeslaTimeoutEndingCassie { get; set; } = "The facility tesla gate. are no longer disabled";
 
         [Description("What should the normal Broadcast duration be?")]
-        public ushort BroadcastDuration = 5;
+        public ushort BroadcastDuration { get; set; } = 5;
 
         [Description("What is the chance for a Gatelockdown?")]
-        public int GatelockdownChance = 15;
+        public int GatelockdownChance { get; set; } = 15;
 
         [Description("What is intervall in which the chance is being checked, to cause a random gatelockdown?")]
-        public int GatelockdownIntervall = 90;
+        public int GatelockdownIntervall { get; set; } = 90;
 
         [Description("What is the minimum duration for a Gatelockdown?")]
-        public int GatelockdownMinDuration = 45;
+        public int GatelockdownMinDuration { get; set; } = 45;
 
         [Description("What is the maximum duration for a Gatelockdown?")]
-        public int GatelockdownMaxDuration = 180;
+        public int GatelockdownMaxDuration { get; set; } = 180;
 
         [Description("What is the minimum duration for a Tesla gate Timeout?")]
-        public int TeslaTimeoutMinDuration = 45;
+        public int TeslaTimeoutMinDuration { get; set; } = 45;
 
         [Description("What is the maximum duration for a Tesla gate Timeout?")]
-        public int TeslaTimeoutMaxDuration = 180;
+        public int TeslaTimeoutMaxDuration { get; set; } = 180;
     }
 }
